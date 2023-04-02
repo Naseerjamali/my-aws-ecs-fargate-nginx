79f632ba2a84bb17bfebf80d00f57530987c324a
naseerjamali <nas@DESKTOP-HQ3IJNU>
2023-04-02 22:21:20 +0530

    added base vpc file

---
+++ Base-infrastructure/aws-vpc-subnet/main.tf
@@ -0,0 +1,100 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr
+  tags = {
+    Name = "${var.name}-vpc"
+  }
+
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.name}-rt"
+  }
+}
+
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+  count             = length(var.private_subnets)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  count                   = length(var.public_subnets)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.second_rt.id
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets)
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.name}-routing-table-private-${format("%03d", count.index+1)}"
+  }
+}
+
+resource "aws_route" "private" {
+  count                  = length(compact(var.private_subnets))
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.main.*.id, count.index)
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+
+resource "aws_nat_gateway" "main" {
+  count         = length(var.private_subnets)
+  allocation_id = element(aws_eip.nat.*.id, count.index)
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  depends_on    = [aws_internet_gateway.gw]
+
+  tags = {
+    Name        = "${var.name}-nat-${format("%03d", count.index+1)}"
+  }
+}
+
+resource "aws_eip" "nat" {
+  count = length(var.private_subnets)
+  vpc = true
+
+  tags = {
+    Name        = "${var.name}-eip-${format("%03d", count.index+1)}"
+  }
+}
